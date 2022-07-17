1159b085a1da61f5803eb8685e0ae27426a965a7
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-17 17:12:04 +0900

    2022.07.17 : 프로젝투 init.

---
+++ src/main/java/com/springmvc2/springmvc2typeconverter/Springmvc2TypeconverterApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc2.springmvc2typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc2TypeconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc2TypeconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc2/springmvc2typeconverter/Springmvc2TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc2.springmvc2typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc2TypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
